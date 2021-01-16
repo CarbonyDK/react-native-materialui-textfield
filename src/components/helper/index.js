95a2af9777b53cd33b93de682287394ed65b881c
Dmitriy Kurovsky <56292904+CarbonyDK@users.noreply.github.com>
2021-01-16 11:29:02 +0200

    Update index.js

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -1,18 +1,9 @@
-import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { View, Animated } from 'react-native';
 
 import styles from './styles';
 
 export default class Helper extends PureComponent {
-
-  static propTypes = {
-    style: Animated.Text.propTypes.style,
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]),
-  };
 
   render() {
     let { children, style, ...props } = this.props;
